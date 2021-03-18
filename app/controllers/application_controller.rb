58106f298de49234ae759e4e27fa2c022f128ef3
Allison _ <allimacq@gmail.com>
2021-03-18 17:18:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,27 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(username: params[:username])
+    
+    if @user != nil
+      session[:user_id] = @user.id
+      redirect to "/account"
+    else
+      erb :error
+    end
 
   end
 
   get '/account' do
-
+    if Helper.
+    
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
+    
+    redirect to '/'
   end
 
 
